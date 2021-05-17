8a51623c32598ad15a20c189240edbf474d435a0
Marco Evair <mmacolluncoc@uni.pe>
2021-05-16 23:44:35 -0500

    Recursive technique

--- functional_programing_with_js.js
+++ functional_programing_with_js.js
@@ -123,6 +123,42 @@ const userLogs = userName => message => console.log(`${userName} -> ${message}`)
 const log = userLogs('grandpa23');
 log("attempted to load 20 fake members")
 
+/* Recursion  is a technique that involves creating functions that recall themself */
+
+const countdown = (value, fn) => {
+  fn(value);
+  return value > 0 ? countdown( value - 1, fn) : value;
+}
+
+countdown(10, value => console.log(value))
+
+
+/* Recursion is a good technique for searching data structures */
+const dan = {
+  type: "person",
+  data: {
+    gender: "male",
+    info: {
+      id: 22,
+      fullname: {
+        first: "Dan",
+        last: "Deacon"
+      }
+    }
+  }
+};
+
+const deepPick = (fields, object = {}) => {
+  const [first, ...remaining] = fields.split(".")
+  return remaining.length
+    ? deepPick(remaining.join("."), object[first])
+      : object[first]
+}
+
+deepPick("data.info.fullname.first", dan); // "Dan"
+
+// COMPOSITIONS
+
 
 
 
