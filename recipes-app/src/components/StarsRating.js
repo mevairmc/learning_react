fb3b661879b4c463ee179721a0a0e4f15e8378a4
Marco Evair <mmacolluncoc@uni.pe>
2021-05-22 07:46:57 -0500

    added useState for selectedStar

--- recipes-app/src/components/StarsRating.js
+++ recipes-app/src/components/StarsRating.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Star from './Star'
 
 const createArray = length => [...Array(length)] // create a array with undefined elements.
 
 export default function StarRating({ totalStar = 5 }){
-  return createArray(totalStar).map((n,i)=> <Star key={i} />)
+  const [selectedStars] = useState(3)
+
+  return (
+    <>
+      {createArray(totalStar).map((n,i)=>
+        <Star key={i} selected={selectedStars > i } />
+      )}
+      <p>{selectedStars} of {totalStar} stars</p>
+    </>
+  )
 }
