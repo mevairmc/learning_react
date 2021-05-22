abecd78e1b9968def05c4aa72d5041ef5b719b08
Marco Evair <mmacolluncoc@uni.pe>
2021-05-22 08:27:53 -0500

    added function to change stars number selected

--- recipes-app/src/components/StarsRating.js
+++ recipes-app/src/components/StarsRating.js
@@ -4,12 +4,18 @@ import Star from './Star'
 const createArray = length => [...Array(length)] // create a array with undefined elements.
 
 export default function StarRating({ totalStar = 5 }){
-  const [selectedStars] = useState(3)
+  const [selectedStars, setSelectedStars] = useState(3)
+
+  // why can I not separate arrow function in other function
 
   return (
     <>
       {createArray(totalStar).map((n,i)=>
-        <Star key={i} selected={selectedStars > i } />
+        <Star
+          key={i}
+          selected={selectedStars > i }
+          onSelect={() =>setSelectedStars(i+1)}
+        />
       )}
       <p>{selectedStars} of {totalStar} stars</p>
     </>
