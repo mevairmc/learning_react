04eceaf584a62ca19c8e17087ae078600d322ec2
Marco Evair <mmacolluncoc@uni.pe>
2021-05-22 18:05:53 -0500

    added new component called Color

--- color-organizer-app/src/components/Color.js
+++ color-organizer-app/src/components/Color.js
@@ -1,1 +1,17 @@
 import React from 'react'
+
+
+export default function Color({color= "white", title = "new color" }) {
+  return(
+    <>
+      <h3>{title}</h3>
+      <div
+        style={{
+          backgroundColor:{color},
+          width: "100%",
+          height: "2rem"
+        }}
+      />
+    </>
+  )
+}
