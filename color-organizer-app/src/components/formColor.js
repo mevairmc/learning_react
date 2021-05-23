31e6823b13d6d9a50e692bfb18d033333a61dd07
Marco Evair <mmacolluncoc@uni.pe>
2021-05-23 00:03:59 -0500

    added controlled component

--- color-organizer-app/src/components/formColor.js
+++ color-organizer-app/src/components/formColor.js
@@ -1,24 +1,41 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 // There are many ways to get information
 // For this component we'll learn to use useRef
 
 export default function AddColorForm(oneNewColor = f => f){
+/*  const txtTitle = useRef()
+  const hexColor = useRef()*/
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
 
-  const txtTitle = useRef()
-  const hexColor = useRef()
   const submit = e => {
     e.preventDefault()
-    const title = txtTitle.current.value
-    const color = hexColor.current.value
+    // const title = txtTitle.current.value
+    // const color = hexColor.current.value
     oneNewColor(title, color)
-    txtTitle.current.value = ""
-    hexColor.current.value = ""
+    // txtTitle.current.value = ""
+    // hexColor.current.value = ""
+    setTitle("")
+    setColor("")
   }
 
   return(
-    <form>
-      <input ref={txtTitle} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+    <form onSubmit={submit}>
+{/*   <input ref={txtTitle} type="text" placeholder="color title..." required />
+      <input ref={hexColor} type="color" required /> */}
+      <input
+        value={title}
+        onChange={event => setTitle(event.target.value)}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input
+        value={color}
+        onChange={event => setColor(event.target.value)}
+        type="color"
+        required
+      />
       <button onClick={submit}>ADD</button>
     </form>
   )
